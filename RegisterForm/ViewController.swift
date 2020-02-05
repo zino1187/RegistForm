27af6255b7602cf3702f49f15e0c72f2a700e749
zino <minzino@hanmail.net>
2020-02-05 09:36:00 +0900

    last commit

--- RegisterForm/ViewController.swift
+++ RegisterForm/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var t_id: UITextField!
+    @IBOutlet weak var t_pass: UITextField!
+    @IBOutlet weak var t_phone: UITextField!
+    
+    @IBAction func btnClick(_ sender: Any) {
+        valueCheck()
+    }
+    
+    func valueCheck(){
+        var msg:String!
+        
+        if t_id.text!.count < 1{
+            msg = "이름을 입력해 주세요"
+            NSLog("아이디를 입력해 주세요!", "")
+            
+        }else if t_pass.text!.count < 1{
+            msg = "비밀번호를 입력해 주세요"
+            NSLog("비밀번호를 입력해 주세요!", "")
+
+        }else if t_phone.text!.count < 1{
+            msg = "휴대폰 번호를 입력해 주세요"
+            NSLog("휴대폰 번호를 입력해 주세요!", "")
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
 }
 
